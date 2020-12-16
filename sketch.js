57cc12780d9da8228be5f8579b59466c60811c4e
FrancisDecorteIII <74832660+FrancisDecorteIII@users.noreply.github.com>
2020-12-16 09:53:21 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,19 +27,16 @@ function draw() {
 if(deformation>180)
 {
   car.shapeColor=color(255,0,0);
-  wall.shapeColor=color(255,0,0);
 }
 
 if(deformation<180 && deformation>100)
 {
   car.shapeColor=color(230,230,0);
-  wall.shapeColor=color(230,230,0);
 }
 
 if(deformation<100)
 {
   car.shapeColor=color(0,255,0);
-  wall.shapeColor=color(0,255,0);
 }
 
 
